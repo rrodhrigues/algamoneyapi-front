2c3738ba77f348e844541f0c1fa1f644877dd265
Renato <rrodriguez@outlook.com.br>
2019-12-09 17:26:46 -0300

    feat add Vetor data in lancamento

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -19,6 +19,6 @@ export class LancamentosPesquisaComponent implements OnInit {
 
   pesquisar() {
     this.lancamentoService.pesquisar()
-      .then(() => null);
+      .then(lancamentos => this.lancamentos = lancamentos);
   }
 }
