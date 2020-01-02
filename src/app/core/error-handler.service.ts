daead4881e1a888e64344d902bd372830f42a796
Renato <rrodriguez@outlook.com.br>
2020-01-02 17:43:53 -0300

    feat: add Challenger Message Error complete

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -11,12 +11,25 @@ export class ErrorHandlerService {
 
     if(typeof errorResponse === 'string')
       msg = errorResponse;
-    else {
+    else if (errorResponse instanceof Response
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        errors = errorResponse.json();
+        msg = errors[0].mensagemUsuario;
+      } catch(e){}
+
+      console.error('Ocorreu um erro', errorResponse);
+    } else {
       msg = 'Erro ao processar serviço remoto, contate o administrador do sistema!!!';
-      console.log('Ocorreu um erro', errorResponse);
+      console.error('Ocorreu um erro', errorResponse);
     }
 
     this.toasty.error(msg);
   }
+
+
 
 }
