0c6a2b2c23b59397a45b046e10433bac5957702d
Renato <rrodriguez@outlook.com.br>
2020-01-16 15:27:06 -0300

    feat: add Create routes for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpModule } from "@angular/http";
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 
 import { LancamentosModule } from "./lancamentos/lancamentos.module";
 import { PessoasModule } from "./pessoas/pessoas.module";
 import { CoreModule } from "./core/core.module";
+import { LancamentosPesquisaComponent } from "./lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component";
+import { LancamentoCadastroComponent } from "./lancamentos/lancamento-cadastro/lancamento-cadastro.component";
+import { PessoasPesquisaComponent } from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent }
+]
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +25,7 @@ import { CoreModule } from "./core/core.module";
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
