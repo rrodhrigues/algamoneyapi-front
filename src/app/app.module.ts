646cf08067c5b16d4c3fff1eca6b832e189aeb8b
Renato <rrodriguez@outlook.com.br>
2020-02-26 16:11:31 -0300

    feat: 18.13 refatorando rotas e criado modulo específico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpModule } from "@angular/http";
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 
 import { LancamentosModule } from "./lancamentos/lancamentos.module";
 import { PessoasModule } from "./pessoas/pessoas.module";
 import { CoreModule } from "./core/core.module";
-import { LancamentosPesquisaComponent } from "./lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component";
-import { LancamentoCadastroComponent } from "./lancamentos/lancamento-cadastro/lancamento-cadastro.component";
-import { PessoasPesquisaComponent } from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
-import { PaginaNaoEncontradaComponent } from "./core/pagina-nao-encontrada.component";
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-]
+import { AppRoutingModule } from "./app-routing-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -30,11 +16,11 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
-    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
