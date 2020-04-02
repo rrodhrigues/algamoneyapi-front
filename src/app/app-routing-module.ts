5aaacda4617d9317537c3cd39b81d53aa8096cb1
Renato <rrodryguis@gmail.com>
2020-04-02 17:53:43 -0300

    refact: Removida imports desnecessarios

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 
-import { PessoasPesquisaComponent } from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
 import { PaginaNaoEncontradaComponent } from "./core/pagina-nao-encontrada.component";
-import { PessoaCadastroComponent } from "./pessoas/pessoa-cadastro/pessoa-cadastro.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
